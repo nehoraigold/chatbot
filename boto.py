ab5c8ef7d5e5e005ee4ae8cd7a0ec282a5e070e1
Ori Gold <nehoraigold@gmail.com>
2018-12-13 18:47:30 +0200

    added weather functionality

--- boto.py
+++ boto.py
@@ -6,31 +6,34 @@ import json, re, random, requests
 
 BAD_WORD_API_NAME = 'nehoraigold'
 BAD_WORD_API_KEY = '8TahuZvfK4tCThrOh2L7LkvaGxUNFa8dcVIMCMuwpNwefDCU'
+WEATHER_API_KEY = '77bcbb75887d4405983155951181312'
 NO_WORDS = ("not", "no", "n't", 'incorrect', 'wrong')
 YES_WORDS = ('yeah', 'yes', 'mhm', 'yep', 'correct', 'right', 'yup')
 GREETING_WORDS = ("hello", "hi", "shalom", "bonjour", "alo", "what's up", "sup", "what is up", "greetings", "hey")
+HEARTBREAK_WORDS = ('hate', 'dislike', 'sucks', 'bad')
 NAME_PREFIXES = ("i am", "i'm", "my name is", "my name's")
 JOKE_WORDS = ('joke', 'funny')
 SONG_WORDS = ('song', 'sing')
+WEATHER_WORDS = ('weather', 'sunny', 'rainy', 'cloudy')
 THANK_WORDS = ("thanks", "thank")
 QUESTION_PATTERN = "[?]+"
 SENTENCE_PATTERN = "([\.\!\?]+)\s*"
 
 
 def handle_response(input):
-    if has_curse_word(input):
-        return handle_curse()
+    # if has_curse_word(input):
+    #     return handle_curse()
     all_sentences = re.split(SENTENCE_PATTERN, input)
     all_words = input.split(' ')
     if asks_for_something(all_words, JOKE_WORDS):
         return get_joke()
-    if asks_for_something(all_words, SONG_WORDS):
+    elif asks_for_something(all_words, SONG_WORDS):
         return get_song()
-    if any([sentence if sentence.lower().startswith(greeting) else None for sentence in all_sentences for greeting in
-            GREETING_WORDS]):
+    elif asks_for_something(all_words, WEATHER_WORDS):
+        return get_weather()
+    elif any(parse_sentences(all_sentences, GREETING_WORDS)):
         return handle_greeting()
-    elif any([word if word.lower().startswith(thank_word) else None for word in all_words for thank_word in
-              THANK_WORDS]):
+    elif any(parse_sentences(all_sentences, THANK_WORDS)):
         return "ok", "You're very welcome."
     elif re.search(QUESTION_PATTERN, input):
         sentences = re.split(SENTENCE_PATTERN, input)
@@ -63,6 +66,12 @@ def asks_for_something(words_in_message, request_triggers):
         [word if word == trigger_word else None for word in words_in_message for trigger_word in request_triggers])
 
 
+def parse_sentences(all_sentences, what_to_parse_for):
+    list_of_words = [sentence if sentence.lower().startswith(trigger_word) else None for sentence in all_sentences for
+                     trigger_word in what_to_parse_for]
+    return list_of_words
+
+
 def get_joke():
     url = 'https://icanhazdadjoke.com'
     headers = {
@@ -77,10 +86,27 @@ def get_joke():
 def get_song():
     SONG_LYRICS = (
         'Who let the dogs out? Who, who, who, who, who?',
-        'Mama, just killed a man. Put a gun against his head. Pulled my trigger, now he\'s dead...',
+        'Mamaaaa, just killed a man. Put a gun against his head. Pulled my trigger, now he\'s dead...',
         'Never gonna give you up. Never gonna let you down. Never gonna turn around and desert you.'
     )
     return 'dancing', random.choice(SONG_LYRICS)
+
+
+def get_weather():
+    url = 'http://api.apixu.com/v1/current.json'
+    params = {
+        'key': WEATHER_API_KEY,
+        'q': 'Tel Aviv'
+    }
+    req = requests.get(url, params)
+    weather_object = json.loads(req.text)
+    city = weather_object['location']['name']
+    country = weather_object['location']['country']
+    temp = weather_object['current']['temp_c']
+    weather_desc = weather_object['current']['condition']['text'].lower()
+    weather_string = "The weather in {0}, {1} is currently {2} degrees Celsius and {3}.".format(city, country,
+                                                                                                str(temp), weather_desc)
+    return 'bored', weather_string
 
 
 def has_curse_word(input):
@@ -105,7 +131,7 @@ def handle_curse():
         "Maybe let's lighten up on the cursing, yeah?",
         "Just so you know, I don't love the swearing."
     )
-    return "cursing", random.choice(CURSE_RESPONSES)
+    return "crying", random.choice(CURSE_RESPONSES)
 
 
 @route('/', method='GET')
